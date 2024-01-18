f0c11e4c0ed7f198465e5ca031daab3f091c68e5
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-18 15:18:32 +0600

    al;dsjf

---
+++ cheapest.js
@@ -0,0 +1,69 @@
+// [45, 21, 6, 98, 56];
+
+const phones = [
+  {
+    name: "Samsung",
+    camera: 12,
+    storage: "32GB",
+    price: 36000,
+    color: "silver",
+  },
+
+  {
+    name: "Walton",
+    camera: 10,
+    storage: "32GB",
+    price: 22000,
+    color: "silver",
+  },
+  {
+    name: "iphone",
+    camera: 7,
+    storage: "32GB",
+    price: 82000,
+    color: "silver",
+  },
+  {
+    name: "Xiaomi",
+    camera: 17,
+    storage: "32GB",
+    price: 52000,
+    color: "silver",
+  },
+  {
+    name: "Oppo",
+    camera: 25,
+    storage: "32GB",
+    price: 20000,
+    color: "black",
+  },
+  {
+    name: "Nokia",
+    camera: 6,
+    storage: "32GB",
+    price: 44000,
+    color: "golden",
+  },
+  {
+    name: "HTC",
+    camera: 10,
+    storage: "32GB",
+    price: 62000,
+    color: "silver",
+  },
+];
+
+function cheapestPhone(phones) {
+  let cheapest = phones[0];
+  for (let i = 0; i < phones.length; i++) {
+    const phone = phones[i];
+    if (phone.price < cheapest.price) {
+      cheapest = phone;
+    }
+  }
+  return cheapest;
+}
+
+const mySelection = cheapestPhone(phones);
+
+console.log(mySelection);
