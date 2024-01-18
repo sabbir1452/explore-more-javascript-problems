1fc1b4462aa32d9ccc0ea83fe2632d96c4ede486
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-18 13:58:26 +0600

    al;dsjf

---
+++ duplicate.js
@@ -0,0 +1,30 @@
+const names = [
+  "abul",
+  "babul",
+  "cabul",
+  "dabul",
+  "ebul",
+  "babul",
+  "abul",
+  "kabul",
+  "gabul",
+  "cabul",
+  "babul",
+  "abul",
+  "abul",
+];
+
+function removeDuplicate(names) {
+  const unique = [];
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    console.log(name);
+    if (unique.includes(name) === false) {
+      unique.push(name);
+    }
+  }
+  return unique;
+}
+
+const uniqueNames = removeDuplicate(names);
+console.log(uniqueNames);
